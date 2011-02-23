4524fc6174b04bda176932ac24b40c299715c3e7
Miki Tebeka <miki.tebeka@gmail.com>
2011-02-22 16:30:17 -0800

    More testing (format fails)

--- test/csvlib_test.clj
+++ test/csvlib_test.clj
@@ -29,3 +29,14 @@
     (write-csv [[1 2 3] [4 5 6]] tmp)
     (is (= (slurp tmp) "1,2,3\n4,5,6\n"))))
 
+(deftest test-write-headers
+  (let [tmp (tempfile)]
+    (write-csv [[1 2 3] [4 5 6]] tmp :headers ["x" "y" "z"])
+    (is (= (slurp tmp) "x,y,z\n1,2,3\n4,5,6\n"))))
+
+(deftest test-write-format
+  (let [tmp (tempfile)
+        format {0 #(format "format %s" %)}]
+    (write-csv [[1 2 3] [4 5 6]] tmp format)
+    (is (= (slurp tmp) "format-1,2,3\nformat-4,5,6\n"))))
+
