d0035f2c503f76307473d4663e5f36270e5a912d
Ben Mabey <ben@benmabey.com>
2011-09-24 21:18:36 -0600

    fixes dep

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :description "A CSV library for Clojure, using csvreader.com"
   :url "https://bitbucket.org/tebeka/csvlib/src"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [com.csvreader/javacsv "2.0"]])
+                 [net.sourceforge.javacsv/javacsv "2.0"]])
