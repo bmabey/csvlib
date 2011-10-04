fcba1cd770ecca418d4c88e614433c062c662bae
Ben Mabey <ben@benmabey.com>
2011-10-04 12:52:05 -0600

    releases my newer version of the project as 0.3.1

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject csvlib "0.3.1-SNAPSHOT"
+(defproject org.clojars.bmabey/csvlib "0.3.1"
   :description "A CSV library for Clojure, using csvreader.com"
-  :url "https://bitbucket.org/tebeka/csvlib/src"
+  :url "https://github.com/bmabey/csvlib"
   :warn-on-reflection true
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [net.sourceforge.javacsv/javacsv "2.0"]])
