b04a09ab6f48e7d162c971535583daf170c37d1f
Ben Mabey <ben@benmabey.com>
2012-02-08 10:49:07 -0700

    releases 0.3.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.bmabey/csvlib "0.3.1"
+(defproject org.clojars.bmabey/csvlib "0.3.2"
   :description "A CSV library for Clojure, using csvreader.com"
   :url "https://github.com/bmabey/csvlib"
   :warn-on-reflection true
